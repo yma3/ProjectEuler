7615a78f71d3472aa257387fc8163e39ea6d285f
Ben Ma <yingfu.ma.96@gmail.com>
2016-03-12 16:35:12 -0500

    Solution

--- 12_Problem/Problem12.cpp
+++ 12_Problem/Problem12.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int div(int x)
+{
+	int limit = x;
+	int factorNum=0;
+	float root = 1;
+	for(int i=1; i<limit; ++i)
+	{
+		//cout << i << endl;
+		if(x%i==0)
+		{
+			limit = x/i;
+			factorNum++;
+			//cout << factorNum << " " << limit << endl;
+		}
+	}
+	root = sqrt(x);
+	if ( root == floor(root) )
+	{	
+		//cout << "div = " << (factorNum*2) - 1 << "triangle = " << x << endl;
+		return (factorNum * 2) - 1;
+	}
+	else
+	{
+		//cout << "div = " << factorNum*2 << "triangle = " << x << endl;
+		return factorNum * 2;
+	}
+}
+
+
+
+
 int main()
 {
-
+	int triangle = 0;
+	for(int naturalCount = 1; div(triangle) <= 500; naturalCount++)
+	{
+		//cout << "div(" << triangle << ")" << endl;
+		triangle = triangle + naturalCount;	
+	}
+	cout << triangle << endl;
+	return 0;
 
 
 
